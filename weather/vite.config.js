6b6bc61844d9616664c862fdf58251229f4caab9
Restarter <nikitovich60@bk.ru>
2026-02-01 19:16:23 +0300

    Debug config file

--- weather/vite.config.js
+++ weather/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/app-weather/',
   server: {
     proxy: {
       // все запросы к /api будут проксироваться на Open-Meteo
