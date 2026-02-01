221d58aac61fd6d0c6c8903f5f61835d31a06ae6
Restarter <nikitovich60@bk.ru>
2026-02-01 19:19:30 +0300

    Debug weather file

--- weather/src/store/weatherSlice.js
+++ weather/src/store/weatherSlice.js
@@ -5,33 +5,35 @@ export const getWeather = createAsyncThunk(
 	'weather/getWeather',
 	async (cityName) => {
 
-	try {	const coordinates = await axios.get(
-			`/api/v1/search?name=${cityName}&count=1&language=ru&format=json`
-		)
-
-		let { latitude, longitude } = coordinates.data.results[0]
-
+		try {
+			const coordinates = await axios.get(
+				`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=ru&format=json`
+			)
 
 
+			let { latitude, longitude } = coordinates.data.results[0]
 
-		let response = await axios.get(
-			`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`
-		) 
 
-		let { temperature_2m } = response.data.hourly
-		let temperature = []
 
-		for (let i = 6; i <= 19; i += 3) {
-			temperature.push({
-				time: `${i}:00`,
-				temp: temperature_2m[i],
-			})
+
+			let response = await axios.get(
+				`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`
+			)
+
+			let { temperature_2m } = response.data.hourly
+			let temperature = []
+
+			for (let i = 6; i <= 19; i += 3) {
+				temperature.push({
+					time: `${i}:00`,
+					temp: temperature_2m[i],
+				})
+			}
+
+			return temperature
+		} catch (e) {
+			throw new Error("Check the city name is correct")
 		}
-
-		return temperature; 
-	}catch(e) {
-		throw new Error("Check the city name is correct")
-	}
 	}
 )
 
@@ -42,17 +44,17 @@ const weatherSlice = createSlice({
 	},
 	extraReducers: (builder) => {
 		builder.addCase(getWeather.fulfilled, (state, action) => {
-			console.log("End loading");
-			console.log(action.payload);
-		});
+			console.log("End loading")
+			console.log(action.payload)
+		})
 		builder.addCase(getWeather.pending, (state, action) => {
-			console.log("Start loading");
-			
+			console.log("Start loading")
+
 		})
 		builder.addCase(getWeather.rejected, (state, action) => {
-			console.log("End loading");
-			console.log(action.error.message);
-			
+			console.log("End loading")
+			console.log(action.error.message)
+
 		})
 	}
 })
